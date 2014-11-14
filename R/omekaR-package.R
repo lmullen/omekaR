395b2390f2294d64b07b14b0d0a7e049fd16c9a5
Lincoln Mullen <lincoln@lincolnmullen.com>
2014-11-13 22:14:20 -0500

    Package documentation

---
+++ R/omekaR-package.R
@@ -0,0 +1,13 @@
+#' omekaR: A client for the Omeka API
+#'
+#' This package provides an API client for the Omeka publishing platform for
+#' scholars, libraries, museums, and archives.
+#'
+#' @docType package
+#' @name omekaR
+#' @title omekaR: A client for the Omeka API
+#' @author Lincoln Mullen, \email{lincoln@@lincolnmullen.com}
+#' @keywords omeka api
+#' @import jsonlite
+#' @import httr
+NULL
