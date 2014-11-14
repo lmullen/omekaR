f95f6b5823a98e352630e5775cff10d3d3f3295b
Lincoln Mullen <lincoln@lincolnmullen.com>
2014-11-13 23:25:43 -0500

    Minor changes to Omeka endpoint documentation

--- R/omeka_endpoint.R
+++ R/omeka_endpoint.R
@@ -1,10 +1,10 @@
-#' Get or set the Omeka endpoint
+#' Get or set the Omeka API endpoint
 #'
 #' Pass a URL to this function to set the Omeka API endpoint for the rest of
 #' your script. Call this function without an argument to get the currently set
 #' endpoint.
 #'
-#' @param endpoint The URL of the Omeka endpoint you are using
+#' @param endpoint The URL of the Omeka API endpoint you are using
 #'
 #' @return The URL of the Omeka endpoint
 #'
@@ -17,7 +17,7 @@ omeka_endpoint <- function(endpoint = NULL) {
 
   if(is.null(endpoint)) {
     endpoint <- Sys.getenv("OMEKA_ENDPOINT")
-    if(endpoint == "") stop("You set an Omeka API endpoint.")
+    if(endpoint == "") stop("You must set an Omeka API endpoint.", call. = FALSE)
   } else {
     Sys.setenv(OMEKA_ENDPOINT = endpoint)
   }
